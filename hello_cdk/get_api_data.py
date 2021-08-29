f1d29094c7229662265fda5ab76972e9bf9c70e5
Tim Cooper <timcooper314@gmail.com>
2021-08-29 19:00:49 +1000

    Added logging in lambda function.

--- hello_cdk/get_api_data.py
+++ hello_cdk/get_api_data.py
@@ -1,17 +1,21 @@
 import os
 import json
+import logging
 from datetime import datetime
 import urllib3
 import boto3
 
+LOGGER = logging.getLogger("SpotifyApiIngestion")
+LOGGER.setLevel(logging.DEBUG)
 
 def put_to_s3_landing(json_object):
     landing_bucket = os.getenv("LANDING_BUCKET_NAME")
-    print(landing_bucket)
     s3_resource = boto3.resource('s3')
     s3_bucket = s3_resource.Bucket(landing_bucket)
     datetime_now = datetime.now().strftime("%Y%m%d")
-    s3_bucket.put_object(Key=f"spotify_data/top_tracks{datetime_now}.json",
+    s3_key = f"spotify_data/top_tracks{datetime_now}.json"
+    LOGGER.debug(f"Putting {s3_key=} into bucket {landing_bucket}")
+    s3_bucket.put_object(Key=s3_key,
                          Body=json.dumps(json_object))
     return
 
@@ -25,6 +29,7 @@ def _get_api_data():
     auth_token = os.getenv("AUTH_TOKEN")
     headers = {"Authorization": f"Bearer {auth_token}"}
     http = urllib3.PoolManager()
+    LOGGER.debug("Getting API data...")
     response = http.request('GET',
                             f"{base_url}{endpoint}",
                             headers=headers,
@@ -33,8 +38,10 @@ def _get_api_data():
 
 
 def get_top_tracks_data():
+    LOGGER.info("Starting lambda execution...")
     json_response = _get_api_data()
     put_to_s3_landing(json_response)
+    LOGGER.info("Finished lambda execution.")
     return "SUCCESS"
 
 
